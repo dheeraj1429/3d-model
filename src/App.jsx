5247dac6eedd6fb721e320d2489f441c2d52860a
dheeraj1429 <dheerajsingh1429@gmail.com>
2024-04-05 23:41:56 +0530

    load texture

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import { Stats, OrbitControls, Circle } from '@react-three/drei'
-import { Canvas, useLoader } from '@react-three/fiber'
+import { useTexture } from '@react-three/drei'
+import { useLoader } from '@react-three/fiber'
+import { Fragment, useLayoutEffect } from 'react'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
+import { beatup } from './textures'
 
 export default function App() {
-  const gltf = useLoader(GLTFLoader, '/models/monkey.glb')
+  const { scene, materials, nodes } = useLoader(GLTFLoader, '/models/taser__taser_gun_model_cs2.glb')
+  // const texture = useLoader(THREE.TextureLoader, beatup[0])
+  const [colorMap, normalMap, roughnessMap, metalnessMap] = useTexture(beatup)
 
-  return (
-    <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
-      <directionalLight position={[3.3, 1.0, 4.4]} intensity={Math.PI} castShadow />
-      <primitive
-        object={gltf.scene}
-        position={[0, 1, 0]}
-        children-0-castShadow
-      />
-      <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
-        <meshStandardMaterial />
-      </Circle>
-      <OrbitControls target={[0, 1, 0]} />
-      <axesHelper args={[5]} />
-      <Stats />
-    </Canvas>
-  )
+  console.log({ materials })
+
+  console.log()
+
+  useLayoutEffect(() => {
+    Object.assign(materials[Object.keys(materials)?.[0]], {
+      metalnessMap: metalnessMap,
+      normalMap: normalMap,
+      roughnessMap: roughnessMap,
+      map: colorMap
+    })
+  }, [scene, nodes, materials, beatup, colorMap, normalMap, roughnessMap, metalnessMap])
+
+  return <primitive object={scene} position={[0, 1, 0]} children-0-castShadow />
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App'
+import { Canvas } from '@react-three/fiber'
+import { Stats, OrbitControls, Circle, useTexture } from '@react-three/drei'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
+      <directionalLight position={[3.3, 1.0, 4.4]} intensity={Math.PI} castShadow />
+      <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
+        <meshStandardMaterial />
+      </Circle>
+      <OrbitControls target={[0, 1, 0]} />
+      <axesHelper args={[5]} />
+      <Stats />
+      <App />
+    </Canvas>
   </StrictMode>
 )
